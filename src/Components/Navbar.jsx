3c94a91b5f0ec0085811b4842c3f4baa2ff9e7bc
mahmud014 <anik955720@gmail.com>
2025-11-10 17:21:33 +0600

    add navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,89 +1,54 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
+  const links = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/about">About</NavLink>
+      </li>
+      <li>
+        <NavLink to="/allreviews">All Reviews</NavLink>
+      </li>
+    </>
+  );
   return (
-    <div className="navbar bg-base-100 shadow-md px-5">
-      {/* Navbar Start */}
-      <div className="navbar-start">
-        {/* Mobile dropdown */}
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <div className="bg-base-100 shadow-md">
+      <div className="navbar container mx-auto ">
+        {/* Navbar Start */}
+        <div className="navbar-start">
+          {/* Mobile dropdown */}
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <FaBars />
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 z-10"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 z-10"
-          >
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <details>
-                <summary>Services</summary>
-                <ul className="p-2">
-                  <li>
-                    <NavLink to="/reviews">Reviews</NavLink>
-                  </li>
-                  <li>
-                    <NavLink to="/favorites">Favorites</NavLink>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <NavLink to="/about">About</NavLink>
-            </li>
-          </ul>
+              {links}
+            </ul>
+          </div>
+          <Link to="/" className="btn-ghost text-2xl font-bold">
+            Dish<span className="text-orange-500">Dive</span>
+          </Link>
         </div>
-        <Link to="/" className="btn btn-ghost text-xl normal-case">
-          DishDive
-        </Link>
-      </div>
 
-      {/* Navbar Center */}
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li tabIndex={0}>
-            <details>
-              <summary>Services</summary>
-              <ul className="p-2 bg-base-100 rounded-box shadow">
-                <li>
-                  <NavLink to="/reviews">Reviews</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/favorites">Favorites</NavLink>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <NavLink to="/about">About</NavLink>
-          </li>
-        </ul>
-      </div>
+        {/* Navbar Center */}
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{links}</ul>
+        </div>
 
-      {/* Navbar End */}
-      <div className="navbar-end">
-        <Link to="/login" className="btn btn-primary">
-          Login
-        </Link>
+        {/* Navbar End */}
+        <div className="navbar-end">
+          <Link to="/login" className="btn btn-primary">
+            Login
+          </Link>
+        </div>
       </div>
     </div>
   );
